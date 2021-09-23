c6dcf90a59a349e47b8b3b04bb0db7ad9d5f5740
Piemontez <piemontez@live.com>
2021-09-23 11:07:34 -0300

    composer

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+CONF_FILE=../wordpress/wp-config.php
+CONF_EX_FILE=../wordpress/wp-config-docker.php
+
+#DATA FOLDERS
+echo "Verificando e criando pastas de dados"
 mkdir -p ../tmp/mysql
 mkdir -p ../tmp/html/upgrade
 mkdir -p ../tmp/html/uploads
 
+#CONFIG
+if [ ! -f "$CONF_FILE" ]; then
+    echo "Criando arquivo de configuração $CONF_FILE."
+    cp $CONF_EX_FILE $CONF_FILE
+fi
+
+#RUN
 cd ../docker
 docker-compose --env-file .env.dev up
