6f9bb2d1aa798229367b8ff0dfbeb7744932aafc
Piemontez <piemontez@live.com>
2021-09-23 10:20:20 -0300

    composer

--- bin/run-prod-docker.sh
+++ bin/run-prod-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 mkdir -p ../tmp/mysql
-mkdir -p ../tmp/html
+mkdir -p ../tmp/html/upgrade
+mkdir -p ../tmp/html/uploads
+
 cd ../docker
 docker-compose --env-file .env.prod up --detach
